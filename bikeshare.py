6ca0ac188d19e7a1445e66b7efa2e09b5a9b0d2b
phuongbl1 <phuongbl088@gmail.com>
2022-11-07 13:47:59 +0700

    Add a print statement

--- bikeshare.py
+++ bikeshare.py
@@ -56,7 +56,7 @@ def get_filters():
     print('-'*40)
     return city, month, day
 
-
+print("Python")
 def load_data(city, month, day):
     """
     Loads data for the specified city and filters by month and day if applicable.
